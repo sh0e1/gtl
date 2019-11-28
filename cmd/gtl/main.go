051ee6a7b8651963d0453925a7b5326f11eeab37
sh0e1 <sh0e1@users.noreply.github.com>
2019-11-29 00:24:23 +0900

    :arrow_up: Update: translate api to v3
    issue=#1

--- cmd/gtl/main.go
+++ cmd/gtl/main.go
@@ -7,49 +7,48 @@ import (
 	"log"
 	"os"
 
-	"cloud.google.com/go/translate"
-	"golang.org/x/text/language"
-	"google.golang.org/api/option"
+	translate "cloud.google.com/go/translate/apiv3"
+	translatepb "google.golang.org/genproto/googleapis/cloud/translate/v3"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	defaultTarget = "ja"
+	defaultSource = "en"
+)
+
+var (
+	apiKey    = os.Getenv("GTL_API_KEY")
+	projectID = os.Getenv("GTL_GCP_PROJECT_ID")
 )
 
 func main() {
-	const (
-		defaultTarget = "ja"
-		defaultSource = "en"
-	)
-
 	var (
 		target = flag.String("target", defaultTarget, "")
 		source = flag.String("source", defaultSource, "")
 	)
 	flag.Parse()
 
-	ctx := context.Background()
-	client, err := translate.NewClient(ctx, option.WithAPIKey(os.Getenv("GTL_API_KEY")))
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "x-goog-api-key", apiKey)
+	ctx = metadata.AppendToOutgoingContext(ctx, "x-goog-user-project", projectID)
+	client, err := translate.NewTranslationClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
 
-	targetLang, err := language.Parse(*target)
+	req := &translatepb.TranslateTextRequest{
+		Contents:           flag.Args(),
+		SourceLanguageCode: *source,
+		TargetLanguageCode: *target,
+		Parent:             "projects/" + projectID,
+	}
+	resp, err := client.TranslateText(ctx, req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sourceLang, err := language.Parse(*source)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	opt := &translate.Options{
-		Source: sourceLang,
-	}
-	translations, err := client.Translate(ctx, flag.Args(), targetLang, opt)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, t := range translations {
-		fmt.Println(t.Text)
+	for _, t := range resp.GetTranslations() {
+		fmt.Println(t.GetTranslatedText())
 	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,13 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.49.0
-	google.golang.org/api v0.14.0
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
+	go.opencensus.io v0.22.2 // indirect
+	golang.org/x/net v0.0.0-20191126235420-ef20fe5d7933 // indirect
+	golang.org/x/oauth2 v0.0.0-20191122200657-5d9234df094c // indirect
+	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11
+	google.golang.org/grpc v1.25.1
 )
